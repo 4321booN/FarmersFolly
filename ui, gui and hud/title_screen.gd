bb878ec64c09fa621579aeae22b8eb09f708d9cd
4321booN <jackdgrabina@gmail.com>
2024-06-05 15:38:28 -0400

    Added a BugReportButton to the TtleScreen, re-enabled full screening

--- ui, gui and hud/title_screen.gd
+++ ui, gui and hud/title_screen.gd
@@ -5,3 +5,7 @@ const world_area: String = "res://world/world_area.tscn"
 
 func _on_world_area_button_pressed() -> void:
 	get_tree().change_scene_to_file(world_area)
+
+
+func _on_bug_report_button_pressed():
+	OS.shell_open("https://forms.gle/Qnh2vkp6ysoavD3cA")
